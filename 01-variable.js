b5ec1a8379b8946295285b652965667316241cbb
Akash Jan <116725881+akashJan@users.noreply.github.com>
2025-02-12 08:54:56 +0530

    Js Variable and Data Types

---
+++ 01-variable.js
@@ -0,0 +1,7 @@
+var age; // Variable Declaration
+console.log(age);
+age = "22"; // Variable Initialization
+console.log("AGE:", age);
+
+var presentCity = Pune; // Variable declaration and initialization in the same line
+console.log("My Present City", presentCity);
---
+++ 01-variableAssign.js
@@ -0,0 +1,33 @@
+var myFullname;
+console.log("My full name is:", myFullname);
+
+myFullname = "Akash Jan";
+console.log("My name is:", myFullname);
+
+myFullname = "Akash Lingappa Jan";
+console.log("My full name is:", myFullname);
+
+var myLove;
+myLove = "I Love only JS";
+console.log(myLove);
+
+var firstName = "Akash";
+console.log("My first name is: ", firstName);
+
+var lastName = "Jan";
+console.log("My last name is: ", lastName);
+
+var age = 27;
+console.log("My age is: ", age);
+
+var address = "Pune";
+console.log("My address is: ", address);
+
+var pinCode = 411048;
+console.log("My pin code is: ", pinCode);
+
+var gender = "Male";
+console.log("My gender is: ", gender);
+
+var isMarried = "NO";
+console.log("Married YES or No: ", isMarried);
