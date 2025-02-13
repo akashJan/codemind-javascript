cc66b82afb17296cd8b165d134b77bce7c64350b
Akash Jan <116725881+akashJan@users.noreply.github.com>
2025-02-13 12:27:43 +0530

    Strings

---
+++ 02-datatypeAssign.js
@@ -0,0 +1,34 @@
+// --------Q1--------
+console.log("Q1:-");
+
+var bankName;
+console.log(
+  `Before initialization: Value- ${bankName} & Type- ${typeof bankName}`
+);
+bankName = "SBI Bank";
+console.log(
+  `After initialization: Value- ${bankName} & Type- ${typeof bankName}`
+);
+
+// --------Q2--------
+console.log("Q2:-");
+var marks = "90%";
+console.log(`Value of marks is: ${marks} & Type of marks is: ${typeof marks}`);
+
+// --------Q3--------
+console.log("Q3:-");
+var isWorking;
+console.log(
+  `Before initialization: Value- ${isWorking} & Type- ${typeof isWorking}`
+);
+isWorking = true;
+console.log(
+  `After initialization: Value- ${isWorking} & Type- ${typeof isWorking}`
+);
+
+// --------Q4--------
+console.log("Q4:-");
+var totalCount = "Hundred and Seven";
+console.log(`a- Value: ${totalCount} & Type: ${typeof totalCount}`);
+totalCount = 107;
+console.log(`b- Value: ${totalCount} & Type: ${typeof totalCount}`);
