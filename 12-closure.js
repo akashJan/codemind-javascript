75b2f4d8db5b35d1412b1089d2de893ab10e6623
Akash Jan <116725881+akashJan@users.noreply.github.com>
2025-03-03 08:33:45 +0530

    Clouser, Callbacks, Callback-Hell, Promises

---
+++ 12-closure.js
@@ -0,0 +1,16 @@
+// Hot interview question
+
+var globalVariable = 100;
+function outer() {
+  let outerFunVariable = 200;
+  let inner = function () {
+    console.log(`Inside inner function`);
+    let innerFunVariable = 300;
+    console.log(innerFunVariable);
+    console.log(outerFunVariable);
+    console.log(globalVariable);
+  };
+  return inner;
+}
+var result = outer();
+result();
